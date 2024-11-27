74a7716dc358907bb1dc0c70daa13b3de0314bd4
JC <juanky201271@gmail.com>
2024-11-27 08:38:36 -0700

    fix: initial work for ZWL Recovery tool

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,15 +4,6 @@
 export const NO_CONNECTION: string = "Could not connect to zcashd";
 
 export default class Utils {
-  // v1 LightwalletD
-  static V1_LIGHTWALLETD: string = "https://lightwalletd.zecwallet.co:1443";
-
-  // v2 LightwalletD
-  static V2_LIGHTWALLETD: string = "https://lwdv2.zecwallet.co:1443";
-
-  // v3 LightwalletD
-  static V3_LIGHTWALLETD: string = "https://lwdv3.zecwallet.co";
-
   static isUnified(addr: string): boolean {
     if (!addr) return false;
     return addr.startsWith("u");
